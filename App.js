668233a21a15a4a961cbf441cc2baa72e0bdb443
José Auyón <josea13p@gmail.com>
2023-05-04 15:59:08 -0600

    Implementacion de pantallas y componentes

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SliderComponent} from './src/components/sliderComponent';
+import { ModalComponent } from './src/components/modalComponent';
 
-export default function App() {
+
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Ir a Galeras"
+        onPress={() => navigation.navigate('Galeras')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Ir a creacion de Galeras"
+        onPress={() => navigation.navigate('Creacion')}
+      />
+    </View>
+  );
+}
+
+function CreacionScreen({ navigation }) {
+  return (
+    <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Ir a Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+
+      <Text>Cantidad de pollos pesados</Text>
+    <SliderComponent></SliderComponent>
+    <Text>Cantidad de alimento proporcionado</Text>
+    <SliderComponent></SliderComponent>
+    <Text>Peso medido</Text>
+    <SliderComponent></SliderComponent>
+    <ModalComponent></ModalComponent>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Galeras" component={DetailsScreen} />
+        <Stack.Screen name="Creacion" component={CreacionScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/components/sliderComponent.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Slider} from '@miblanchard/react-native-slider';
+import {AppRegistry, StyleSheet, View, Text} from 'react-native';
+
+export class SliderComponent extends React.Component {
+    state = {
+        value: 0.2,
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Slider
+                    value={this.state.value}
+                    onValueChange={value => this.setState({value})}
+                />
+                <Text>Value: {this.state.value}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 4,
+        marginLeft: 10,
+        marginRight: 10,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+});
+
+AppRegistry.registerComponent('SliderComponent', () => SliderComponent);
