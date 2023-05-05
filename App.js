d0f3bc0e6910a98c8cb0ee90875fea49d8b34fbb
Diego Hernández <dahernandezsilvestre@gmail.com>
2023-05-04 21:18:20 -0600

    Commit con la vista de galeras del trabajador y añadir componente de CardGalera

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, ScrollView, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SliderComponent} from './src/components/sliderComponent';
 import { ModalComponent } from './src/components/modalComponent';
+import CardGalera from './src/components/CardGalera';
 
 
 
 function HomeScreen({ navigation }) {
+  const navigateToGaleras = () => {
+    navigation.navigate('Galeras');
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Ir a Galeras"
-        onPress={() => navigation.navigate('Galeras')}
-      />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#D3D3D3' }}>
+      <StatusBar barStyle="light-content" backgroundColor="#fff" />
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+        <CardGalera galera='Galera 14' ca='red' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 8' ca='green' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 6' ca='orange' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 9' ca='red' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 18' ca='green' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 17' ca='red' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 13' ca='orange' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 1' ca='red' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 9' ca='orange' navigateToGaleras={navigateToGaleras}/>
+        <CardGalera galera='Galera 15' ca='green' navigateToGaleras={navigateToGaleras}/>
+      </ScrollView>
     </View>
   );
 }
@@ -58,7 +72,20 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}
+            options={{
+              headerTitle: 'Galeras y tareas pendientes',
+              headerStyle: {
+                backgroundColor: '#fff',
+                elevation: 0, // para eliminar la sombra en Android
+              },
+              headerTintColor: '#000',
+              headerTitleStyle: {
+                fontSize: 25,
+              },
+              headerTitleAlign: 'center', // centrar el título
+            }}
+          />
         <Stack.Screen name="Galeras" component={DetailsScreen} />
         <Stack.Screen name="Creacion" component={CreacionScreen} />
       </Stack.Navigator>
