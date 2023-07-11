2a930500e4eb25e4d8d009d9e9aeeb47c71b014a
José Auyón <josea13p@gmail.com>
2023-07-10 21:27:56 -0600

    se añadi+o initial screen

---
+++ src/screens/InitialScreen/styles.js
@@ -0,0 +1,10 @@
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+})
+
+export default styles
