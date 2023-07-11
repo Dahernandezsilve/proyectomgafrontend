d46db0b8efbca2258b0040f6754f7613b11e9ecd
José Auyón <josea13p@gmail.com>
2023-07-10 20:09:32 -0600

    ultimos cambios a pantallas y componentes

---
+++ src/components/SliderComponent/styles.js
@@ -0,0 +1,10 @@
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+})
+
+export default styles
