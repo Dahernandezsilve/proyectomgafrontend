a4f30f61449fc87eed2d5dc6905ffc04191a961e
Diego Hernández <dahernandezsilvestre@gmail.com>
2023-08-01 19:33:05 -0600

    Some fixes

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,18 @@
-module.exports = function (api) {
-  api.cache(true)
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          alias: {
-            '@components': './src/components',
-            '@img': './scr/img',
-          },
-        },
-      ],
-    ],
-  }
-}
+    module.exports = function (api) {
+      api.cache(true)
+      return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['./src'],
+              alias: {
+                '@components': './src/components',
+                '@img': './scr/img',
+              },
+            },
+          ],
+        ],
+      }
+    }
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: 'jest-expo',
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+};
+  
