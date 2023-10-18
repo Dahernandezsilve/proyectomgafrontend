2579d3bfbfdb60da1c3e40b6d23505a80cf045cc
Diego Hernández <dahernandezsilvestre@gmail.com>
2023-10-17 20:27:53 -0600

    loading workers

--- src/screens/PersonalScreen/PersonalScreen.jsx
+++ src/screens/PersonalScreen/PersonalScreen.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
-  View, Text, TextInput, ScrollView,
+  View, Text, ScrollView,
 } from 'react-native'
 import PropTypes from 'prop-types'
 import { CardGaleraAdmin, HeaderInformation, BottomTabNavigation } from '../../components'
 import styles from './styles'
+import useApi from '../../hooks/useApi/useApi'
 
 const galera = ['Jose Fernandez', 'Esteban Escalante', 'Raul Vasquez']
 const cantidadAlimento = ['4356 7890', '9234 5632', '1208 6684']
@@ -13,11 +14,39 @@ const decesos = ['Encargado de Lote', 'Encargado de Lote', 'Encargado de Lote']
 
 const PersonalScreen = ({ navigation }) => {
   const lotes = ['20', '1', '2']
+  const [workers, setWorkers] = useState()
   const [activeTab, setActiveTab] = useState(lotes[0])
+  const [response, , handleRequest] = useApi()
 
   const navigateToGaleras = async () => {
     navigation.navigate('Creacion')
   }
+
+  const handleObtainWorkers = () => {
+    try {
+      // eslint-disable-next-line no-shadow
+      handleRequest('GET', '/obtainTrabajadores')
+      // Haz algo con la respuesta aquí si es necesario
+    } catch (error) {
+      console.error('Error al obtener los trabajadores:', error)
+    }
+  }
+
+  useEffect(() => {
+    console.log('response', workers)
+  }, [workers])
+
+  useEffect(() => {
+    if (response.data !== undefined && response.data !== null) {
+      if (response.data.length > 0) {
+        setWorkers(response.data)
+      }
+    }
+  }, [response])
+
+  useEffect(() => {
+    handleObtainWorkers()
+  }, [])
 
   const tabs = [
     {
@@ -52,23 +81,25 @@ const PersonalScreen = ({ navigation }) => {
             <Text style={styles.workerTitle}>Personal</Text>
           </View>
 
-          {galera.map((nombre, index) => (
-            <CardGaleraAdmin
-              // eslint-disable-next-line react/no-array-index-key
-              key={index}
-              ca="#FFFFFF"
-              msgCA=""
-              numberCA={null}
-              customValues={{
-                galera: nombre,
-                cantidadAlimento: cantidadAlimento[index],
-                pesado: pesado[index],
-                decesos: decesos[index],
-                navigateToGaleras,
-              }}
-              customTitles={['Nombre:', 'Telefono:', 'Direccion:', 'Puesto:']}
-            />
-          ))}
+          {workers !== undefined && workers.length > 0 ? (
+            workers.map((worker, index) => (
+              <CardGaleraAdmin
+      // eslint-disable-next-line react/no-array-index-key
+                key={index}
+                ca="#FFFFFF"
+                msgCA=""
+                numberCA={null}
+                customValues={{
+                  galera: worker.nombre,
+                  cantidadAlimento: worker.telefono,
+                  pesado: worker.direccion,
+                  decesos: worker.puesto,
+                  navigateToGaleras,
+                }}
+                customTitles={['Nombre:', 'Telefono:', 'Direccion:', 'Puesto:']}
+              />
+            ))
+          ) : null}
 
         </View>
       </ScrollView>
