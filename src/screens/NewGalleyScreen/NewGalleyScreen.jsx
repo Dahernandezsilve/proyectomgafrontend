9690b281dc074dc6c71eb0681d26c69a044678e0
XaviAlvarado18 <alv21188@uvg.edu.gt>
2023-11-19 00:41:08 -0600

    Start

--- src/screens/NewGalleyScreen/NewGalleyScreen.jsx
+++ src/screens/NewGalleyScreen/NewGalleyScreen.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   View, Text, TextInput, ScrollView,
 } from 'react-native'
 import PropTypes from 'prop-types'
 import { CardGaleraAdmin, HeaderInformation, BottomTabNavigation } from '../../components'
 import styles from './styles'
+import useApi from '../../hooks/useApi/useApi'
 
-const galera = ['Jose Fernandez', 'Esteban Escalante', 'Raul Vasquez']
-const cantidadAlimento = ['4356 7890', '9234 5632', '1208 6684']
-const pesado = ['Oratorio', 'Cuilapa', 'Barberena']
-const decesos = ['Encargado de Lote', 'Encargado de Lote', 'Encargado de Lote']
 
 const NewGalleyScreen = ({ navigation }) => {
   const lotes = ['Mi granja', 'Asignacion', 'Crear galera']
   const [activeTab, setActiveTab] = useState(lotes[2])
+  const [response, , handleRequest] = useApi()
+  const cant_lotes = ['1', '2', '3', '4']
+  const [verLotes, setLotes] = useState(cant_lotes[0])
 
   const navigateToGaleras = async () => {
     navigation.navigate('Creacion')
@@ -34,6 +34,46 @@ const NewGalleyScreen = ({ navigation }) => {
     },
   ]
 
+  // eslint-disable-next-line no-shadow
+  const handleObtainGalleysPerLote = async verLotes => {
+    const body = {
+      numLote: parseInt(verLotes, 10),
+    }
+
+    console.log('body', body)
+
+    // Realiza una solicitud POST a la ruta de Flask
+    const response = await handleRequest('POST', '/galerasAdmin', body)
+    console.log('Mensaje:', response)
+    if (Array.isArray(response.data) && response.data.length > 0) {
+      const galleysData = response.data.map(galley => ({
+        numeroGalera: galley.numeroGalera,
+        typeChicken: galley.typeChicken,
+        existence: galley.existence
+      }));
+      console.log('Datos de las galeras:');
+      console.log(galleysData);
+
+      // Accede solo al número de galera
+      const numerosGalera = galleysData.map(galley => galley.numeroGalera);
+
+      setGalleysPerLote(galleysData); // Asegúrate de tener una función setWorkersPerLote para establecer los datos en tu estado
+    } 
+    //console.log("Datos:", galleysPerLote)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await handleObtainGalleysPerLote('1'); // Llama a la función con el valor de lote deseado
+      } catch (error) {
+        console.error('Error al obtener galeras:', error);
+      }
+    };
+    fetchData(); // Llama a la función al montar el componente
+  }, []); 
+  
+
   const [activeTabb] = useState(2)
 
   return (
@@ -51,55 +91,10 @@ const NewGalleyScreen = ({ navigation }) => {
       <ScrollView>
         <View style={styles.container}>
 
-          <View style={styles.formGroup}>
-            <Text style={styles.label}>No. galera:</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Ingrese el número de galera"
-              keyboardType="numeric"
-              maxLength={2}
-            />
-          </View>
-
-          <View style={styles.formGroup}>
-            <Text style={styles.label}>Tipo de población:</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Ingrese el tipo de población"
-            />
-          </View>
-
-          <View style={styles.formGroup}>
-            <Text style={styles.label}>Cantidad de pollos:</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Ingrese la cantidad de pollos"
-              keyboardType="numeric"
-              maxLength={4}
-            />
-          </View>
-
           <View style={styles.rectangle}>
             <Text style={styles.workerTitle}>Asignar trabajador</Text>
           </View>
 
-          {galera.map((nombre, index) => (
-            <CardGaleraAdmin
-              // eslint-disable-next-line react/no-array-index-key
-              key={index}
-              ca="#FFFFFF"
-              msgCA=""
-              numberCA={null}
-              customValues={{
-                galera: nombre,
-                cantidadAlimento: cantidadAlimento[index],
-                pesado: pesado[index],
-                decesos: decesos[index],
-                navigateToGaleras,
-              }}
-              customTitles={['Nombre:', 'Telefono:', 'Direccion:', 'Puesto:']}
-            />
-          ))}
 
         </View>
       </ScrollView>
